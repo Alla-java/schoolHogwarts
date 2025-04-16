5dc41de7d039a6fe2fc439fd5c6b2df94602cc9c
Alla-java <goneman@yandex.ru>
2025-04-16 23:46:23 +0300

    Выполнено ДЗ по теме Параллельные стримы

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.IntStream;
+
 @RestController
 public class InfoController {
 
@@ -13,5 +15,13 @@ public class InfoController {
     @GetMapping("/port")
     public String getPort () {
         return "Application is running on port: " + serverPort;
+    }
+
+    //Эндпоинт который будет возвращать целочисленное значение
+    @GetMapping("/sum-parallel")
+    public long getSumParallel() {
+        return IntStream.rangeClosed(1, 1_000_000)
+                .parallel()
+                .reduce(0, Integer::sum);
     }
 }
